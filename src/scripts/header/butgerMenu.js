7d8ede39cf96ad8e6f80849529115360fb233d19
I-BroneX-I <vladislav.2022.markevich@gmail.com>
2023-12-15 14:18:53 +0200

    create mobile menu

--- src/scripts/header/butgerMenu.js
+++ src/scripts/header/butgerMenu.js
@@ -2,8 +2,19 @@
 
 const burgetBtn = document.querySelector('#burgetBtn');
 const ul = document.querySelector('#bodyNav');
+const topNav = document.querySelector('.topNav');
+const topForm = document.querySelector('#topForm');
 
-burgetBtn.addEventListener('click', function(event){
+burgetBtn.addEventListener('click', function (event) {
     this.classList.toggle('activeBurger');
     ul.classList.toggle('activeUl');
+    topNav.classList.toggle('openNavBody');
+    topForm.classList.toggle('activeForm');
+
+    if (document.body.style.overflow == 'hidden') {
+        document.body.style.overflow = 'scroll'
+    } else {
+        document.body.style.overflow = 'hidden';
+    }
+
 })
